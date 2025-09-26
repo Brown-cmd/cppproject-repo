ddc3687976f67fc05a9e05bee54892c5f9c9563c
brown-cmd <browngervasorie@gmail.com>
2025-09-27 01:56:52 +0300

    fibonnacci sequence using recursive functions

--- fibbonacci.cpp
+++ fibbonacci.cpp
@@ -1,34 +1,29 @@
 #include <iostream>
 using namespace std;
 
-int fibonnacci (int n)
-{
-    if (n <= 0)
-    {
-        return (0);
-    }
-    if (n <= 1)
-    {
-        return(1);
-    }
-    else
-    {
-        return{(fibonnacci(n-1) + fibonnacci(n-2))};
-    }
-    
-}
-
+int fibonnacci(int n);
+int result;
 
 int main ()
 {
-    int start;
+    int start=1;
     int end;
-    cout << "Enter the start of the sequence: ";
-    cin >> start;
     cout << "End of sequence: ";
     cin >> end;
     fibonnacci(end);
     cout << "the fibonnacci sequence from " << start << " till " << end << " is " << fibonnacci;
 
     return (0);
+}
+
+int fibonnacci(int n)
+{
+    if (n<2)
+    {
+        return(n);
+    }
+    else
+    {
+        return ((fibonnacci(n-1) + fibonnacci(n-2)));
+    }
 }
